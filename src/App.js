1f017cf84a82786ac3615dc66abc12ef9abbf968
Delay Coderz <delaycoderz@gmail.com>
2021-10-05 20:15:08 +0700

    Change Icon Header

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ function App() {
       <center>
         <img 
           className="app__headerImage"
-          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+          src="../public/Postagram.png"
           alt="Instagram Logo"
         /> 
         </center>
